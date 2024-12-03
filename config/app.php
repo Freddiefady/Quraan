b16812c632d8abc7d3cfffd5386d8d7333f1ee28
Freddie Fady <dodofady49@gmail.com>
2024-12-03 19:05:16 +0200

    First Commit

--- config/app.php
+++ config/app.php
@@ -194,7 +194,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\checkSettingProvider::class,
+        // App\Providers\checkSettingProvider::class,
         Mcamara\LaravelLocalization\LaravelLocalizationServiceProvider::class,
 
     ],
